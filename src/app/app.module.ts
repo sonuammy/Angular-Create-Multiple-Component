8f5429116c9fe6cb9380ee8c879c85e79f44e51f
sonuammy <amsusingh990@gmail.com>
2019-02-08 14:29:04 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndiaComponent } from './india/india.component';
+import { UsaComponent } from './usa/usa.component';
+import { DelhiComponent } from './delhi/delhi.component';
+import { MumbaiComponent } from './mumbai/mumbai.component';
+import { NewyorkComponent } from './newyork/newyork.component';
+import { WashingtonComponent } from './washington/washington.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndiaComponent,
+    UsaComponent,
+    DelhiComponent,
+    MumbaiComponent,
+    NewyorkComponent,
+    WashingtonComponent
   ],
   imports: [
     BrowserModule
